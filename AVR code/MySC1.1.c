5dd3b6f1a7a0761970c67e58e9fe9e6ab7cb197e
simarsingh24 <simarsingh24@gmail.com>
2016-01-22 13:12:15 +0530

    .\AVR code

--- AVR code/MySC1.1.c
+++ AVR code/MySC1.1.c
@@ -17,7 +17,7 @@ int temp_array[10];
 
 void pwm_init()
 {
-	DDRD |= _BV(PD4);						//Setting output
+	DDRB |= _BV(PB6);						//Setting output
 	TCCR1A |= _BV(COM1A1) | _BV(COM1B1);	//16 bit T/C
 	TCCR1B |= _BV(WGM13) | _BV(CS11);		//Phase and Frequency correct mode, prescaler 8
 	
@@ -33,13 +33,13 @@ void startMusic()							//start data trans. with Z
 void playMusic()					//8-bit tempo value
 {
 				
-				int song[][62] = {	{Fis4, E4, G4, E4, Fis4, D4,E4,D4,
+				int song[2][62] = {	{Fis4, E4, G4, E4, Fis4, D4,E4,D4,
 								Fis4, E4, G4, E4, Fis4, D4, E4,p,
 								Fis4, E4, G4, E4, Fis4, D4, E4, D4,
 								Fis4, E4, G4, E4, Fis4, D4, E4, p,		//32 till here
 								Fis4, G4, B5, p, c5, p, B5, p,  C5, p, B5, p, C5, p, B5,		//15
 								Fis4, G4, A5, p, B5, p, A5, p, B5,p, A5,p,G4, Fis4, E4,p},	//16
-				
+								
 								{
 									E3,Fis3,Fis3,Fis3,p,E3,E3,E3,p,	//9
 									E3,G3,G3,G3,p,Fis3,Fis3,Fis3,p,
@@ -51,8 +51,7 @@ void playMusic()					//8-bit tempo value
 								}								
 								
 								};
-										
-				
+									
 	ICR1 = song[song_no][current_song_node];
 	current_song_node++;
 }
@@ -64,7 +63,7 @@ void stopMusic()					//stop data trans. with Z
 		current_song_node = 0;
 }
 
-ISR(USART_RXC_vect)
+ISR(USART0_RX_vect)
 {
 	tempo = (int)USART_Receive(0);
 	if(k<8)
@@ -100,11 +99,11 @@ ISR(USART_RXC_vect)
 			_delay_ms(10);
 			
 		}
-		while(UCSRA & (1<<RXC))
+		while(UCSR0A & (1<<RXC))
 		{
 			unsigned char dummy;
-			if(UDR!=200)
-			dummy = UDR;
+			if(UDR0!=200)
+			dummy = UDR0;
 		}
 		//USART_Transmitchar('202',0);
 		//ICR1 = 0;
@@ -121,9 +120,9 @@ int main()
 	USART_InterruptEnable(0);
 	OCR1B = 80;
 	
-	TCCR1B |=1<<CS10|1<<WGM12;
-	TIMSK|=1<<OCIE1A;
-	OCR1A=32000;
+	TCCR3A |=1<<CS10|1<<WGM12;
+	TIMSK|=1<<OCIE3A;
+	OCR3A=32000;
 	
 	while(1)
 	{		
@@ -131,6 +130,12 @@ int main()
 return 0;
 }
    
-ISR(TIMER1_COMPA_vect)
-{EEPROM_write(0x0A,tempo);
+ISR(TIMER3_CAPT_vect)
+{if(song_no==0)
+	EEPROM_write(0x0A,tempo);
+else if (song_no==1)
+	EEPROM_write(0x256A,tempo);
+else if (song_no==2)
+	EEPROM_write(0x513A,tempo);
+else EEPROM_write(0x769A,tempo);
 }	
